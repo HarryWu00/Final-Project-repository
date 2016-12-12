e8a8b1e886ec85b9acdf2ff893c7927cd6b0a381
HarryWu00 <8hwu@stpatrick.org>
2016-12-12 10:55:12 -0600

    Item class

---
+++ item.js
@@ -0,0 +1,14 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(n,p,st){
+    this.name = n;
+    this.price = p;
+    this.shippingTime = st;
+  }
+
+
+//Create your three test items below this comment.
+  var tablet = new Item ("Ipad",1234,3);
+  var phone =  new Item ("Mate9",765,2);
+  var book = new Item ("Book", 15,8);
+  console.log()
