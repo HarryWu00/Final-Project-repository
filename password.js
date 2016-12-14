bf0db1d157587049b790634352704eb43ae26f93
HarryWu00 <8hwu@stpatrick.org>
2016-12-14 11:24:48 -0600

    Day 3

---
+++ password.js
@@ -0,0 +1,38 @@
+class Password{
+  //The first part of the class.
+  constructor(p, pk){
+    this.publickey = p;
+    this.privatekey = pk;
+  }
+
+
+  //Instance functions below this comment.
+  validPublicKey(){
+    if(this.publickey.length >= 8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey[4] == "-" && this.privatekey[9] == "-"){
+      if(){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else{
+      return false;
+    }
+  }
+
+  //Static function below this comment.
+static makePrivateKey()
+  var key=[];
+  var limit = 14;
+  for(var p=0, p<this.privatekey.length,p++)
+
+}
